17fb0d987b4bc6747ec8c84f514623c8779c7d31
qleap <ivan.kniaziuk@gmail.com>
2021-08-17 21:44:39 +0300

    Finish first step

---
+++ app/config/server.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 8080),
+  url: 'http://localhost',
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9da3045c5de805880ade5a0ed13d7d51'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd8a0a808-82b9-46c4-9d10-9e2df92a51a7'
+};
